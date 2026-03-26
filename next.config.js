b8cb2e2bcdd9ded5e56c09a4c8aa11219035b966
shahidanwar2 <shahidanwar995595@gmail.com>
2026-03-26 12:05:14 +0530

    fix versions and config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Isse TypeScript errors build ke waqt ignore ho jayenge
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Isse ESLint warnings bhi ignore ho jayengi
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
